d21c911738b4e635f270f146cf9172020cc3dc74
Aleksey <aleksey9247@yandex.ru>
2025-03-23 17:06:51 +0300

    revert

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,54 +1,10 @@
-"use strict";
-
+"use strict"
 function solveEquation(a, b, c) {
-  const d = Math.pow(b, 2) - 4 * a * c; // Вычисляем дискриминант
-  const roots = [];
-
-  if (d < 0) {
-    // Корней нет
-    return roots;
-  } else if (d === 0) {
-    // Один корень
-    const x = -b / (2 * a);
-    roots.push(x);
-  } else {
-    // Два корня
-    const x1 = (-b + Math.sqrt(d)) / (2 * a);
-    const x2 = (-b - Math.sqrt(d)) / (2 * a);
-    roots.push(x1, x2);
-  }
-
-  return roots;
+  let arr = [];
+  
+  return arr;
 }
-
-// Пример использования:
-console.log(solveEquation(1, -3, 2));  // [2, 1]
-console.log(solveEquation(1, 2, 1));   // [-1]
-console.log(solveEquation(1, 0, 1));   // []
-
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  const monthlyPercent = (percent / 100) / 12; // месячная процентная ставка
-  const creditBody = amount - contribution;   // тело кредита
-
-  if (creditBody <= 0) {
-    return 0;
-  }
-
-  // Расчёт ежемесячного платежа по формуле
-  const monthlyPayment = creditBody * (monthlyPercent + (monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1)));
-
-  // Общая сумма выплат
-  const totalPayment = monthlyPayment * countMonths;
-
-  // Округляем до двух знаков после запятой и возвращаем число
-  return Number(totalPayment.toFixed(2));
+  
 }
-
-console.log(calculateTotalMortgage(10, 0, 50000, 12));    // 52749.53
-console.log(calculateTotalMortgage(10, 1000, 50000, 12)); // 51694.54
-console.log(calculateTotalMortgage(10, 0, 20000, 24));    // 22149.56
-console.log(calculateTotalMortgage(10, 1000, 20000, 24)); // 21042.09
-console.log(calculateTotalMortgage(10, 20000, 20000, 24)); // 0
-console.log(calculateTotalMortgage(10, 0, 10000, 36));    // 11616.19
-console.log(calculateTotalMortgage(15, 0, 10000, 36));    // 12479.52
